6cfba204b949fc454afde066d754252977232e18
lego_jm <wjddb10@gmail.com>
2023-11-20 16:42:05 +0900

    'router_init'

---
+++ src/components/HotelHeader.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HotelHeader() {
+  return <div>HotelHeader</div>;
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import RoomDetail from "./pages/RoomDetail";
+import Reservation from "./pages/Reservation";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      { path: "/rooms", element: <Rooms /> },
+      { path: "/rooms/:roomId", element: <RoomDetail /> },
+      { path: "/reservation/:roomId", element: <Reservation /> },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
